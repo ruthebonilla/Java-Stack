bef3be91ea25debd6d237a7f173e2bee52cd2e69
Ruth Bonilla <ruthebonilla01@gmail.com>
2022-04-04 20:53:12 -0400

    First Java Program

---
+++ fundamentals/first-java-program /Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My name is Ruth Bonilla \n I am 20 years old \n My hometown is Winchester, VA");
+    }
+}
