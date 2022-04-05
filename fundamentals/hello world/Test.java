4107b85e441b3cf2d4391e1615dba94f3f401b3a
Ruth Bonilla <ruthebonilla01@gmail.com>
2022-04-04 20:27:01 -0400

    Hello World

---
+++ fundamentals/hello world/Test.java
@@ -0,0 +1,10 @@
+// File name must be the same as the class name
+// Every file IS a class
+public class Test {
+    // The file or class with the exat method signature below is always the launching point of the application
+    // You cannot run a Java program without it
+    public static void main(String[] args) {
+        // For now, all your code goes here
+        System.out.println("Hello World!");
+    }
+}
